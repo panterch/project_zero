c3d8d1e2673789fc8b03fc2487c1ef6a4714789a
seb <seb@tao.panter.local>
2010-08-26 17:17:01 +0200

    rails new project_zero -J -dsqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectZero::Application.config.secret_token = '180bbd83b460f6bf2590d7f8872f33196afcb11e8e351f8f7aa0d923287834f07a799579a1b69f0e6eae977bd4de91fb76c31417ed0c8a862aabfcf6fb4c2335'
