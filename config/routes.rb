08845465c8b1c916d077f943814acd11ae32d514
seb <seb@balaton.local>
2011-02-14 17:01:25 +0100

    complete crud for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ProjectZeroDevise::Application.routes.draw do
+  root :to => 'posts#index'
   resources :posts
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -11,5 +11,25 @@ describe PostsController do
       response.code.should eq('200')
     end
   end
+  
+  context 'POST on create' do
+    before(:each) { post :create, :post => Factory.attributes_for(:post) }
+    it('responds with a redirect') { response.code.should eq('302') }
+    it('creates a new post') { assigns(:post).should_not be_a_new_record }
+  end
+
+  context 'persisted post' do
+    let(:post) { Factory(:post) }
+    context 'GET on edit' do
+      before(:each) { get :edit, :id => post }
+      it('respons with success'){ response.code.should eq('200') }
+    end
+    context 'GET on index' do
+      before(:each) { get :index }
+      it('respons with success'){ response.code.should eq('200') }
+      it('assigns posts'){ assigns(:posts).should eq([post]) }
+    end
+  end
+
 
 end
