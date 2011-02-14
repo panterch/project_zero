349e5c3f77094062de5b74107aa05b3bb9d01fea
seb <seb@balaton.local>
2011-02-14 17:12:23 +0100

    attr protected as default

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,4 +11,10 @@ describe Post do
     post.should be_valid
   end
 
+  it "should allow mass assignement for title and body" do
+    post = Post.new(:title => 'title', :body => 'body')
+    post.title.should be_present
+    post.body.should be_present
+  end
+
 end
