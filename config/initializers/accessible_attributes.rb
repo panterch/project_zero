cdd05ea1c82a0dc5953f89f681cf52f0f811d35a
seb <seb@panter.ch>
2011-03-07 14:07:34 +0100

    update mass assignement lockdown documentation

--- config/initializers/accessible_attributes.rb
+++ config/initializers/accessible_attributes.rb
@@ -1,15 +1,16 @@
 # make all attributes protected by default
+#
+# to open up certain attributes in you model use:
+# attr_accessible :your_attribute
+#
+# if you prefer the opposite behaviour (all attributes open, just certain
+# attributes locked down) just call attr_protected in your model:
+#
+# attr_protected :single_protected_attribute
+#
+# To open up all attributes can be dangerous. One example is an update of a
+# foreign key attribute on a record which could lead to injection of records
+# into another users objects.
 class ActiveRecord::Base
   attr_accessible
-  attr_accessor :accessible
-
-  private
-
-  def mass_assignment_authorizer
-    if accessible == :all || Array(accessible).include?(:all)
-      self.class.protected_attributes
-    else
-      super + (accessible || [])
-    end
-  end
 end
